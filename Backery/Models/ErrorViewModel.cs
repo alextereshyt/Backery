199c34fe4d7a3e2a165b9d7a02c2d7179bbeb01e
alextereshyt <alextereshyt@gmail.com>
2022-11-19 14:44:45 +0200

    Добавьте файлы проекта.

---
+++ Backery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Backery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
